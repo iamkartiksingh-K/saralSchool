0db036143039b36bac6767fe88a3ca9346ddad5a
Kartik Singh <kartiksinghk2001@gmail.com>
2024-07-16 15:52:54 +0530

    basic course page

--- saral_school/src/app/api/users/signup/route.ts
+++ saral_school/src/app/api/users/signup/route.ts
@@ -1,38 +1,38 @@
 import axios from "axios";
-import {  NextResponse } from "next/server";
-import type { NextRequest} from "next/server";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 import { v4 as uuidv4 } from "uuid";
 
 export async function POST(request: NextRequest) {
-	const data = await request.json();
-	const username = uuidv4();
-	try {
-		const response = await axios.post(
-			`${process.env.STRAPI_URL}/api/auth/local/register`,
-			{
-				...data,
-				username,
-			}
-		);
-		console.log(response);
-		return NextResponse.json({
-			message: "Confirmation email sent",
-		});
-	} catch (error: any) {
-		console.log("Error registering user", error.code);
-		if (error.code === "ECONNREFUSED") {
-			return NextResponse.json(
-				{
-					message: "Unable to connect to backend",
-				},
-				{ status: 500 }
-			);
-		}
-		return NextResponse.json(
-			{
-				message: "Email is already taken!",
-			},
-			{ status: 400 }
-		);
-	}
+  const data = await request.json();
+  const username = uuidv4();
+  try {
+    const response = await axios.post(
+      `${process.env.STRAPI_URL}/api/auth/local/register`,
+      {
+        ...data,
+        username,
+      },
+    );
+    console.log(response);
+    return NextResponse.json({
+      message: "Confirmation email sent",
+    });
+  } catch (error: any) {
+    console.log("Error registering user", error);
+    if (error.code === "ECONNREFUSED") {
+      return NextResponse.json(
+        {
+          message: "Unable to connect to backend",
+        },
+        { status: 500 },
+      );
+    }
+    return NextResponse.json(
+      {
+        message: "Email is already taken!",
+      },
+      { status: 400 },
+    );
+  }
 }
--- saral_school/src/app/courses/[course_id]/page.tsx
+++ saral_school/src/app/courses/[course_id]/page.tsx
@@ -4,7 +4,10 @@ import axios from "axios";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { CloudinaryVideoPlayer } from "next-cloudinary";
-
+import { User, BookOpen } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { CldVideoPlayer } from "next-cloudinary";
+import "next-cloudinary/dist/cld-video-player.css";
 export default function ShowCourse() {
   const { course_id } = useParams();
   const [course, setCourse] = useState<courseType>(defaultCourse);
@@ -22,15 +25,38 @@ export default function ShowCourse() {
   }, [course_id]);
   return (
     <div className="h-dvh">
-      <div className="container pt-8">
-        <div>
-          <h1 className="text-3xl font-semibold">{course.name}</h1>
-          <p className="text-sm font-medium">
-            By {course.instructor?.fullName}
-          </p>
+      <div className="bg-slate-800 text-white h-1/4 py-5">
+        <div className="container flex flex-col justify-between h-full">
+          <div>
+            <h1 className="text-3xl font-semibold">{course.name}</h1>
+            <div className="flex items-center space-x-5 mt-3">
+              <p className="text-sm font-semibold flex items-center">
+                <User className="h-4 w-4 mr-2" /> {course.instructor?.fullName}
+              </p>
+              <p className="text-sm font-semibold flex items-center">
+                <BookOpen className="h-4 w-4 mr-2" />
+                {course.lectures?.length} Lectures
+              </p>
+            </div>
+          </div>
+
+          <Button variant="secondary" className="w-24 font-semibold rounded-sm">
+            Buy Course
+          </Button>
         </div>
-        <div className="mt-10">
-          <h2 className="text-2xl font-semibold">About</h2>
+      </div>
+      <div className="container pt-8">
+        <div className="mt-8 grid grid-cols-2">
+          <div>
+            <h2 className="text-2xl font-semibold">About</h2>
+            <p>{course.description}</p>
+          </div>
+          <div>
+            <CldVideoPlayer width="1920" height="1080" src="something" />
+          </div>
+        </div>
+        <div className="mt-12">
+          <h2 className="text-2xl font-semibold">Lectures</h2>
         </div>
       </div>
     </div>
